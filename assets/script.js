baec9a1fd4eafc30bba0e4d01496b8325407941a
Kristina Faye <kglinoga@gmail.com>
2022-06-24 10:32:08 -0700

    basic structure. HTML, CSS and JS linked

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+// javascript
+// // randomly choose computer choice
+//     //either r, p or s
+//     store in variable
+
+var compChoice = ["r", "p", "s"]
+
+// collect users choice (R,P, S)
+//     ask user for R,P or S via pop up
+//     EDGE CASE: user doesn't pick R P or s   
+    
+// figure out result
+//     win: R:P, P:R, S:P
+//     loss: s:r, r:P, p:s
+//     tie: s:s, etc.
+
+// log stats
+//     3 possible outcomes
+//     create variables for win, loss, and tie
+//     update appropriate variable based on result
+//     display to user
+
+// keep playing until quit
+
+// an array of like 4 names
+
+// var randomNum = Math.random()
+
+// var numTimesLength = randomNum * names.Length
+
+// var roundDown = Math.floor(numTimesLength)
+
+// console.log(names[roundDown])
+// //the below is ACTUALLY what you'll likely use. 
+// console.log(names[Math.floor(Math.random()*names.length)])
+
+// **when you get nested parenthesis, start with the smallest nested doll and work out. 
+
