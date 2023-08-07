f0b1ad27d0476ea79edbaf37708425cbae5c362d
Kristina Faye <kglinoga@gmail.com>
2023-08-07 11:52:23 -0700

    worked through code all at once, it works! Glad for logic review

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,43 @@ var wins = 0;
 var ties = 0;
 var losses = 0;
 
-// options for computer
 var options = ["R", "P", "S"];
 
-// this is the game
 var playGame = function () {
-    
     var userChoice = window.prompt("Ender R, P, or S:");
 
-    // if user pressed cancel, immediately end function
-    if (!userChoice) {
-        return;
-    }
+    userChoice = userChoice.toUpperCase();
 
+    // this chooses a random number between 0-2
     var index = Math.floor(Math.random() * options.length);
+
+    // this uses that random number to choose an item in the options array, then calls it computer choice.
     var computerChoice = options[index];
 
-    window.alert("The computer chose " + computerChoice);
+    window.alert("The compuer chose " + computerChoice);
+
+    if (userChoice === computerChoice) {
+        ties++;
+        window.alert("It's a tie!")
+    } else if (
+        (userChoice === "R" && computerChoice === "S") ||
+        (userChoice === "P" && computerChoice === "R") ||
+        (userChoice === "S" && computerChoice === "P")
+    ) {
+        wins++;
+        window.alert("You Win!");
+    } else {
+        losses++;
+        window.alert("Better luck next time!");
+    };
+
+    window.alert("Stats: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties);
+
+    var playAgain = window.confirm("Play Again?");
+
+    if (playAgain) { 
+        playGame();
+    }
 }
+
+playGame();
